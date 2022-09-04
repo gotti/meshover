b052708b49b6fc45a8f2e36dd2173652de707f19
gotti <gotti@mail.gotti.dev>
2022-09-04 14:16:16 +0900

    add proto for statusmanager

--- spec/ip.pb.validate.go
+++ spec/ip.pb.validate.go
@@ -905,3 +905,103 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = AddressCIDRValidationError{}
+
+// Validate checks the field values on ASN with the rules defined in the proto
+// definition for this message. If any rules are violated, the first error
+// encountered is returned, or nil if there are no violations.
+func (m *ASN) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ASN with the rules defined in the
+// proto definition for this message. If any rules are violated, the result is
+// a list of violation errors wrapped in ASNMultiError, or nil if none found.
+func (m *ASN) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ASN) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Number
+
+	if len(errors) > 0 {
+		return ASNMultiError(errors)
+	}
+
+	return nil
+}
+
+// ASNMultiError is an error wrapping multiple validation errors returned by
+// ASN.ValidateAll() if the designated constraints aren't met.
+type ASNMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ASNMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ASNMultiError) AllErrors() []error { return m }
+
+// ASNValidationError is the validation error returned by ASN.Validate if the
+// designated constraints aren't met.
+type ASNValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ASNValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ASNValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ASNValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ASNValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ASNValidationError) ErrorName() string { return "ASNValidationError" }
+
+// Error satisfies the builtin error interface
+func (e ASNValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sASN.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ASNValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ASNValidationError{}
